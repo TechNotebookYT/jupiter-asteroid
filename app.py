63c390c84d83d9e7405838677ef9b5ae22c3351d
Pranav Bala <technotebook@yahoo.com>
2021-11-20 17:13:49 -0600

    added login parser & basic vars

--- app.py
+++ app.py
@@ -6,14 +6,23 @@ import time
 desktop_useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4482.0 Safari/537.36 Edg/92.0.874.0"
 mobile_useragent = "Mozilla/5.0 (Linux; Android 11; SM-N986B) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/88.0.4324.152 Mobile Safari/537.36" # Galaxy Note 20 Ultra
 
-with open('userpass.txt') as passdoc:
-    username1 = passdoc.readline()[:-1]
-    pass1 = passdoc.readline()[:-1]
-    username2 = passdoc.readline()[:-1]
-    pass2 = passdoc.readline()[:-1]
-    username3 = passdoc.readline()[:-1]
-    pass3 = passdoc.readline()[:-1]
+# File path for the chromedriver
+chrome_path = r'/usr/local/bin/chromedriver'
 
-print(username1, pass1)
-print(username2, pass2)
-print(username3, pass3)
+# Vars
+logins = []
+
+# You Need a file called userpass.txt with the usernames and passwords on different lines
+with open('userpass.txt') as passdoc:
+    # Puts each set of logins and passwords in a list [[L1, P1], [L2, P2] ...]
+    for line in passdoc:
+        line = line[:-1]
+        if str(line)[-3:] == "com":
+            logins.append([line])
+            print(logins)
+        else:
+            logins[len(logins)-1].append(line)
+            print(logins)
+
+
+
