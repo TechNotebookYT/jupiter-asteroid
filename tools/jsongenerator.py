4f5154e9d0191c4b500c1c10ba0fdde381d9c973
Pranav Bala <technotebook@yahoo.com>
2022-02-05 11:24:35 -0600

    added jsongenerator tool to simplify json adding process for xpath and link

---
+++ tools/jsongenerator.py
@@ -0,0 +1,17 @@
+name = input("name: ")
+data = input("data: ")
+
+if input("type (x or u): ") == 'u':
+    print(f'"{name}": "{data}",')
+    print(f"url_data['{name}']")
+else:
+    jsonstr = ''
+    for char in data:
+        if char == '"':
+            jsonstr += "\\" + char
+        else:
+            jsonstr += char
+    print(f'"{name}": "{jsonstr}",')
+    print(f"element_data['{name}']")
+
+
