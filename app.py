09632dd97eb628278f193bcfd3049364ebdd3797
Pranav Bala <technotebook@yahoo.com>
2022-08-13 14:00:06 -0500

    cleaned up/reorganized code, removed deprecated functions, added error notifications

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import arrow # TIme library
+import arrow # Time library
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -11,15 +11,6 @@ import argparse
 import os
 from country_list import countries_for_language
 import json
-
-# Options
-headless_mode = True
-
-# RandomWords Specific
-# r = list(requests.get(
-#     "https://random-word-api.herokuapp.com/word?number=50").json()) OLD
-r = str(requests.get(
-    'https://raw.githubusercontent.com/lorenbrichter/Words/master/Words/en.txt').content).split(r'\n')
 
 
 # Useragents to change browser
@@ -50,7 +41,8 @@ lastName = args.last_name
 
 if args.debug: # Checks if debug mode turned on
     headless_mode = False
-
+else:
+    headless_mode = True
 
 # You Need a file called userpass.txt with the usernames and passwords on different lines
 # !!!!!! Make sure you have a newline on the last line of your userpass.txt file
@@ -71,7 +63,10 @@ with open(f'{current_path}/elements.json') as jfile:
 with open(f'{current_path}/urls.json') as jfile:
     url_data = json.load(jfile)
 
-# Takes in the platform and whether or not it will be headless to create a webdriver
+# Random word list
+r = str(requests.get(
+    'https://raw.githubusercontent.com/lorenbrichter/Words/master/Words/en.txt').content).split(r'\n')
+# Takes uses platform & headless status to create webdriver
 def create_driver(mobile, headless):
     if mobile:
         print('Mobile Browser')
@@ -80,7 +75,7 @@ def create_driver(mobile, headless):
         
     # Selects the Correct User Agent
     if mobile:
-        # Picks from 2 different useragents
+        # Picks from many different useragents
         useragent = random.choice(mobile_useragents)
     else:
         useragent = desktop_useragent
@@ -94,6 +89,7 @@ def create_driver(mobile, headless):
         opts.add_argument('--headless')  # Turns on headless mode
         opts.add_experimental_option(
             'excludeSwitches', ['enable-logging'])  # Turns off verbose logging
+    
     # Creates webdriver with options and returns it
     return webdriver.Chrome(service=Service(
         ChromeDriverManager().install()), options=opts)
@@ -106,7 +102,7 @@ def login(driver_login, acct):
     user, passwd = acct  # Sets Username and Password values from acct list
 
     while not logged_in:
-        print("Attempting Login...")
+        print("Attempting Login...   ", user)
         driver_login.get('https://login.live.com/')
         time.sleep(3)  # Wait for page to load
 
@@ -122,55 +118,14 @@ def login(driver_login, acct):
                 '//*[@id="idSIButton9"]').click()  # sign in button
             time.sleep(3)
 
-        logged_in = login_check_v2(driver_login)
-        print("Logged In: ", logged_in)
+        logged_in = login_check(driver_login)
+        # print("Logged In: ", logged_in) ## Debug Code
         # print("User: ", user) ## Debug Code
         # print("Pass: ", passwd) ## Debug Code
     print("Login Successful: ", user)  # Prints Email to the Screen
 
 
 def login_check(check_driver):
-    msft_acct_check = False  # This is the check for the microsoft account page
-    bg_acct_check = False  # This is the check for the bing account page
-
-    # Checks if name is on the webpage
-    check_driver.get('https://account.microsoft.com/')
-    time.sleep(2)
-    account_body = check_driver.find_element(By.TAG_NAME,
-        "body").text  # All of the text on the webpage
-
-    check_driver.get(url_data['time_search'])
-    search_engine_fullpage = check_driver.page_source.encode('utf-8')
-
-    if (((firstName in account_body) or (lastName in account_body))):
-        msft_acct_check = True
-
-    signin_tries = 0
-    
-    while(bg_acct_check == False and signin_tries <= 3):
-        if ((firstName in str(search_engine_fullpage)) or (lastName in str(search_engine_fullpage))):
-            bg_acct_check = True
-        else:
-            signin_tries += 1
-            if len(check_driver.find_elements(By.XPATH, '//*[@id="id_a"]')) > 0:
-                check_driver.find_element(By.XPATH, '//*[@id="id_a"]').click()
-            elif len(check_driver.find_elements(By.XPATH, element_data['mobile_bing_hamburger'])) > 0:
-                check_driver.find_element(By.XPATH, element_data['mobile_bing_hamburger']).click()
-                time.sleep(2)
-                if (len(check_driver.find_elements(By.XPATH, element_data['mobile_post_ham_pfp_xpath']))) > 0:
-                    check_driver.find_element(By.XPATH, 
-                        element_data['mobile_post_ham_pfp_xpath']).click()
-                    time.sleep(2)
-                    # input()
-                    bg_acct_check = True
-                    print('hamburger checked')
-            time.sleep(1)
-            search_engine_fullpage = check_driver.page_source.encode('utf-8')
-
-    return bg_acct_check and msft_acct_check
-
-
-def login_check_v2(check_driver):
     # Checks if name is on the webpage
     check_driver.get('https://account.microsoft.com/')
     time.sleep(2)
@@ -247,102 +202,7 @@ def check_name_on_page(sourceCode):
     return False
 
 
-# Checks the num of points earned on the present day !**{OLD}**!
-def DEPRECATED_check_num_pts(check_driver):
-    pcsearch_complete = False
-    mobilesearch_complete = False
-    edgesearch_complete = False
-
-    check_driver.get('https://account.microsoft.com/')
-    time.sleep(3)
-    rewards_btn = check_driver.find_element(By.XPATH, 
-        r'//*[@id="navs"]/div/div/div/div/div[4]/a')
-    rewards_btn.click()
-    time.sleep(3)
-    check_driver.get("https://rewards.microsoft.com/pointsbreakdown")
-    check_driver.switch_to.window(
-        check_driver.window_handles[0])  # Switches to first tab
-    time.sleep(3)
-    points = []
-
-    lvl2Field = 'Mobile search'  # This is only present if the account is at level 2
-
-    bodyTag = check_driver.find_element(By.TAG_NAME,
-        "body")  # Returns everything in the body tag
-
-    # Checks if the account is at level 2 status and sets the lvl2 boolean accordingly
-    if lvl2Field in bodyTag.text:
-        lvl2 = True
-        # print("LVL2") ## Debug Code
-    else:
-        lvl2 = False
-        # print("LVL1") ## Debug Code
-
-    if lvl2:
-        # PC Pts
-        pc_search_pts = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-        max_pcsearch = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text
-
-        max_pcsearch = (max_pcsearch[int(max_pcsearch.index('/'))+2:])
-        pcsearch_complete = (int(pc_search_pts) == int(max_pcsearch))
-
-        # Edge Pts
-        edge_search_pts = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[3]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-        max_edgesearch = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[3]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text
-
-        max_edgesearch = (max_edgesearch[int(max_edgesearch.index('/'))+2:])
-        edgesearch_complete = (int(edge_search_pts) == int(max_edgesearch))
-
-        # Mobile Pts
-        mobile_search_pts = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-        max_mobilesearch = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text
-
-        max_mobilesearch = (
-            max_mobilesearch[int(max_mobilesearch.index('/'))+2:])
-        mobilesearch_complete = (
-            int(mobile_search_pts) == int(max_mobilesearch))
-
-        # Adds the point data to the list
-        points.append(
-            [pcsearch_complete, mobilesearch_complete, edgesearch_complete])
-        points.append([int(pc_search_pts), int(
-            mobile_search_pts), int(edge_search_pts)])
-        points.append([int(max_pcsearch), int(
-            max_mobilesearch), int(max_edgesearch)])
-    else:
-        # PC Pts
-        pc_search_pts = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-        max_pcsearch = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text
-
-        max_pcsearch = (max_pcsearch[int(max_pcsearch.index('/'))+2:])
-        pcsearch_complete = (pc_search_pts == max_pcsearch)
-
-        # Edge Pts
-        edge_search_pts = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b').text
-        max_edgesearch = check_driver.find_element(By.XPATH, 
-            '//*[@id="userPointsBreakdown"]/div/div[2]/div/div[2]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]').text
-
-        max_edgesearch = (max_edgesearch[int(max_edgesearch.index('/'))+2:])
-        edgesearch_complete = (edge_search_pts == max_edgesearch)
-
-        # Adds the point data to the list
-        points.append([pcsearch_complete, edgesearch_complete])
-        points.append([int(pc_search_pts), int(edge_search_pts)])
-        points.append([int(max_pcsearch), int(max_edgesearch)])
-
-    return points
-
-
-def updated_check_num_pts(check_driver):
+def check_num_pts(check_driver):
     points = []
 
     check_driver.get(url_data["points_url"])
@@ -500,8 +360,8 @@ def main():
             driver = create_driver(False, headless_mode)  # Creates the desktop driver
             login(driver, logins[i])  # Logs in on desktop driver
             # Checks the number of points and adds it to pts list
-            pts = updated_check_num_pts(driver)
-            print(pts)  # Prints out the pts list
+            pts = check_num_pts(driver)
+            print("Remaining Pts: [desktop, edge, mobile]: ", pts)  # Prints out the pts list
 
             pc_complete = (pts[0] == 0)
             edge_complete = (pts[1] == 0)
@@ -510,6 +370,7 @@ def main():
             complete_challenge_1(driver)
 
             if len(pts) == 3:
+                # Lvl 2 Account Code
                 mobile_complete = (pts[2] == 0)
                 tries = 0
                 while (not(pc_complete and edge_complete and mobile_complete)) and (tries < 3):
@@ -520,18 +381,19 @@ def main():
                         random_searches(driver, ((pts[0]+pts[1])/5)+1)
                     if not mobile_complete:
                         mobilePts(headless_mode, (pts[2]/5)+1, logins[i])
-                    pts= updated_check_num_pts(driver)
-                    print(pts)
+                    pts= check_num_pts(driver)
+                    print("Remaining Pts: [desktop, edge, mobile]: ", pts)
                     tries+=1
                 driver.quit()
             else:
+                # Lvl 1 Acct code
                 tries = 0
                 while (not(pc_complete and edge_complete)) and (tries < 3):
                     pc_complete = (pts[0] == 0)
                     edge_complete = (pts[1] == 0)
                     random_searches(driver, ((pts[0]+pts[1])/5)+1)
-                    pts = updated_check_num_pts(driver)
-                    print(pts)
+                    pts = check_num_pts(driver)
+                    print("Remaining Pts: [desktop, edge, mobile]: ", pts)
                     tries += 1
                 driver.quit()
             
@@ -550,4 +412,9 @@ def main():
                 print("All targets reached... Exiting")
                 exit()
 
-main()
+try:
+    main()
+except:
+    from plyer import notification
+    notification.notify(title="ERROR", message="Jupiter-Asteroid: Check Logs")
+    # WARNING: LOGS NOT IMPLEMENTED YET
