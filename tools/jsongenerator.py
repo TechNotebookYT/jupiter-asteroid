60cb855d078d9b282998def68ec361dcb53a38fc
Pranav Bala <technotebook@yahoo.com>
2022-08-15 22:32:19 -0500

    added comments to jsongenerator

--- tools/jsongenerator.py
+++ tools/jsongenerator.py
@@ -1,9 +1,12 @@
 name = input("name: ")
 data = input("data: ")
 
+# Url json
 if input("type (x or u): ") == 'u':
+#   Printing URL JSON
     print(f'"{name}": "{data}",')
     print(f"url_data['{name}']")
+# Xpath Json
 else:
     jsonstr = ''
     for char in data:
@@ -11,6 +14,7 @@ else:
             jsonstr += "\\" + char
         else:
             jsonstr += char
+#     Printing XPATH
     print(f'"{name}_xpath": "{jsonstr}",')
     print(f"element_data['{name}_xpath']")
 
